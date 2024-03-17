3db049e1a45c5a624f3ae97abf54aed9ec386825
voorhees410 <72003865+stef440-th@users.noreply.github.com>
2024-03-17 15:36:05 +0400

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,5 +1,5 @@
 #main script for training and building  neural network
-using Flux, using Images, using ImageSegmentation
+using Flux, using Images, using ImageSegmentation, using ObjectDetector
 # define a function 'main' which is a convolutionnal neural network for detection and segementation as mask r cnn
 function main
   #neural network
